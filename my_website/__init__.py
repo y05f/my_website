8020a5bb89f9b3637567a93df8bb9f92c74d3c41
y05f <ysf.safi@gmail.com>
2022-10-27 10:52:13 +0200

    Correcting style with flake8

--- my_website/__init__.py
+++ my_website/__init__.py
@@ -1,31 +1,28 @@
-import os
-
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template
 from flask import make_response
 from io import BytesIO
-
 
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__)
-    
+
     @app.route('/')
     def index():
         return render_template('index.html')
 
-    @app.route('/projects.html', methods=['GET',])
-    def projects():
-        return render_template('projects.html')
-    
+    # @app.route('/projects.html', methods=['GET',])
+    # def projects():
+    #     return render_template('projects.html')
+
     @app.route('/resume.pdf')
     def get_resume():
-        with open('./my_website/static/resume.pdf','rb') as pdf_file:
+        with open('./my_website/static/resume.pdf', 'rb') as pdf_file:
             binary_pdf = BytesIO(pdf_file.read())
             response = make_response(binary_pdf)
             response.headers['Content-Type'] = 'application/pdf'
             response.headers['Content-Disposition'] = \
                 'inline; filename=%s.pdf' % 'safi_resume'
             return response
-        
+
     return app
